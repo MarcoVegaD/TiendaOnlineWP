e4a194d2c9c4c968eca4c5b743ae29ec0350b77c
MarcoVega <vegadiaz.marcoantonio@gmail.com>
2021-06-08 13:24:21 -0500

    Añadida base de tema hijo

---
+++ wp-content/themes/newsmag-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+add_action ( "wp_enqueue_scripts", "llamar_estilos" );
+
+function llamar_estilos() {
+    wp_enqueue_style( "parent-style", get_template_directory_uri() . "/style.css" );
+}
